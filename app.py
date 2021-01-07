0a02390571d0eed6b6abb9da1654dc454dba297f
Mairi <mairi.mcqueer@gmail.com>
2021-01-07 16:22:42 +0000

    Working on cookies

--- app.py
+++ app.py
@@ -20,31 +20,38 @@ client = boto3.client('rds')
 token = client.generate_db_auth_token(DBHostname=ENDPOINT, Port=PORT, DBUsername=USER, Region=REGION)
 
 app = Flask(__name__)
-
-# Database functions
 def GetAnswers():
     try:
-        conn  = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=token)
+        conn  = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=PASSWORD)
         cur = conn.cursor()
         answers = cur.execute("SELECT right, wrong1, wrong2, wrong3 FROM Questions;")
         cur.close()
         conn.commit()
         conn.close()
+        return(answers)
 
     except Exception as e:
         print("Error:{}".format(e))
     
-    return(answers)
 def ShuffleAnswers(question):
-    answers = GetQuestions()
+    answers = GetAnswers()
     shuffledAns = answers[question-1] # because the array is 0 indexed
     random.shuffle(shuffledAns)
+    print(shuffledAns)
     return(shuffledAns)
+
+def SetCookie():
+    if request.method == "POST":
+        questionNum +=1
+        response = make_response(render_template("quiz.html"))
+        response.set_cookie("QuestionNumber", questionNum)
+        return(response)
 
 # Web app functions
 @app.route('/') #127.0.0.1:5000
 def index(): #homepage
-    return render_template('index.html')
+    ShuffleAnswers(0)
+    return render_template("index.html")
 
 @app.route('/', methods=['POST'])
 def getUsername():
@@ -52,34 +59,38 @@ def getUsername():
         conn  = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=PASSWORD)
         cur = conn.cursor()
         username = request.form['username']
-        cur.execute("INSERT INTO HighScores (username) VALUES (%s);"(username))
+        cur.execute("INSERT INTO HighScores (username) VALUES ('%s');" % username)
         cur.close()
         conn.commit()
         conn.close()
 
     except Exception as e:
         print("Error:{}".format(e))
-    
+
     return("")
 
 @app.route('/quiz', methods= ['POST'])
 def quiz():
-    return render_template('quiz.html')  
+    if not request.cookies.get("Number"):
+        response = make_response("Creating Cookie")
+        response.set_cookie("Question Number", num)
+
+    return render_template("quiz.html")  
 
 @app.route('/highScores')
 def highScores():
     try:
-        conn  = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=token)
+        conn  = psycopg2.connect(host=ENDPOINT, port=PORT, database=DBNAME, user=USER, password=PASSWORD)
         cur = conn.cursor()
-        data = cur.execute("SELECT * FROM HighScores ORDER BY DESC LIMIT 10;")
+        data = cur.execute("SELECT * FROM HighScores ORDER BY score DESC LIMIT 10;")
         cur.close()
         conn.commit()
         conn.close()
+        return render_template('highscores.html', highscores = data)
 
     except Exception as e:
         print("Error:{}".format(e))
-    
-    return render_template('highscores.html', highscores = data)
+        return("Error:{}".format(e))
 
 if __name__ == '__main__':
     app.run(debug=True, host='0.0.0.0')
